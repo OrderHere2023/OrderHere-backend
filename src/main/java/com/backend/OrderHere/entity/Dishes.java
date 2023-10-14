d471d08c25657b6f646e49a83a01b3547292d697
cansibi <69489820+cansibi@users.noreply.github.com>
2023-10-15 05:12:57 +1030

    Dishes

--- src/main/java/com/backend/OrderHere/entity/Dishes.java
+++ src/main/java/com/backend/OrderHere/entity/Dishes.java
@@ -6,10 +6,8 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
-import java.time.OffsetDateTime;
-import java.util.UUID;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
 
 @Entity
 @Getter
@@ -18,61 +16,39 @@ import java.time.LocalDateTime;
 @DynamicInsert
 @Table(name = "Dishes")
 public class Dishes {
-    public static enum DishAvailability {
-        AVAILABLE("Available"),
-        SOLD_OUT("Sold Out");
-        
-        private final String displayName;
-
-        DishAvailability(String displayName) {
-            this.displayName = displayName;
-        }
-
-        public String getDisplayName() {
-            return displayName;
-        }
-    }
     @Id
     @Column(name = "dish_id", nullable = false)
-    private UUID DishId;
+    private Integer DishId;
 
     @Column(name = "dish_name", nullable = false)
     private String DishName;
 
-    //description
     @Column(name = "description", nullable = true)
     private String Description;
     
-    //price
     @Column(name = "price", nullable = false,precision = 10, scale = 2)
-    private BigDecimal Price;
+    private Double Price;
 
-    //image url
     @Column(name = "image_url", nullable = true)
     private String ImageUrl;
 
-    //category
     @Column(name = "category", nullable = false)
     private String Category;
 
-    //rating
-    @Column(name = "rating", nullable = true,precision = 10, scale = 2)
-    private BigDecimal Rating;
 
-    //restaurant id
+    @Column(name = "rating", nullable = true,precision = 10, scale = 2)
+    private Double Rating;
+
     @Column(name = "restaurant_id", nullable = false)
     private Integer RestaurantId;
 
-    //availability
     @Enumerated(EnumType.STRING)
     @Column(name = "availability", nullable = false)
-    private DishAvailability Availability;
+    private Boolean Availability;
 
-    //created at
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime CreatedAt;
+    private ZonedDateTime CreatedAt;
 
-    //updated at
     @Column(name = "updated_at", nullable = false)
-    private LocalDateTime UpdatedAt;
+    private ZonedDateTime UpdatedAt;
 }
