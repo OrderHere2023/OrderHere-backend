d9ea6c718b3686802964dfd034d7384134f6df7f
Zachary1317 <117606781+Zachary1319@users.noreply.github.com>
2023-11-28 11:45:46 +1100

    remove unnecessary code

--- src/main/java/com/backend/orderhere/controller/v1/DishController.java
+++ src/main/java/com/backend/orderhere/controller/v1/DishController.java
@@ -49,7 +49,6 @@ public class DishController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void createDish(@Valid @ModelAttribute DishCreateDto dishCreateDto) {
-//      System.out.println(dishCreateDto);
         dishService.createDish(dishCreateDto);
     }
 }
